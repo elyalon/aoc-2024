1b8e00d18515d6788aabfd77df9afcfda13be919
Ely <elyalon345@gmail.com>
2024-12-05 17:56:16 +0200

    5.2 hacky solution

--- 05.ts
+++ 05.ts
@@ -10,24 +10,48 @@ const updates = updatesInput!
   .split("\n")
   .map((line) => line.split(",").map(Number));
 
-const correctUpdates = updates.filter((update) =>
-  rules.every((rule) => !ruleBroken(rule, update))
-);
+const fixedUpdates = updates.filter((update) => {
+  let fixed = false;
 
-const result = correctUpdates
+  for (const rule of rules) {
+    if (updateNeededFix(rule, update)) fixed = true;
+  }
+
+  return fixed;
+});
+
+for (let i = 0; i < 100; i++) {
+  fixedUpdates.forEach((update) => {
+    for (const rule of rules) {
+      updateNeededFix(rule, update);
+    }
+  });
+}
+
+const result = fixedUpdates
   .map((update) => update[Math.floor(update.length / 2)]!)
   .reduce((acc, x) => acc + x, 0);
 
 console.log(result);
 
-function ruleBroken(rule: [number, number], update: number[]): boolean {
+function updateNeededFix(rule: [number, number], update: number[]): boolean {
   const idx = update.findIndex((page) => page === rule[0]);
 
   if (idx === -1) return false;
 
   const rest = update.slice(0, idx);
 
-  if (!rest.includes(rule[1])) return false;
+  const restIdx = rest.findIndex((page) => page === rule[1]);
+
+  if (restIdx === -1) return false;
+
+  console.log(`Update before fix of rule [${rule}]: ${update}`);
+
+  const tmp = update[idx]!;
+  update[idx] = update[restIdx]!;
+  update[restIdx] = tmp;
+
+  console.log(`Update after fix of rule [${rule}]: ${update}`);
 
   return true;
 }
